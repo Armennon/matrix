437cd4f67f61ee0e3cd20af7f7c01107fee9d23a
Armennon <57568491+Armennon@users.noreply.github.com>
2019-11-21 21:40:18 +0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -20,4 +20,16 @@ module.exports = {
             return null;
         }
     }
+    printMatrix: function(matrix){
+        if(matrix == null || matrix == undefined){
+            return;
+        }
+        result = "";
+        for(let i = 0; i < matrix.length; i++){
+            for(let j = 0; j < matrix[i].length; j++){
+                result += matrix[i][j] + " ";
+            }
+            result += "\n";
+        }
+        return result;
 }
